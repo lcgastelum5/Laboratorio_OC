707211fcbc070cb1fd5c241c9005cab07891735d
lcgastelum5 <leonardo.cordova.gastelum@uabc.edu.mx>
2025-10-22 08:21:30 +0000

    Guardado

--- P6/P6.asm
+++ P6/P6.asm
@@ -1,5 +1,9 @@
 %include "../LIB/pc_iox.inc"
 
+section .data
+
+	variable DW 0
+
 section	.text
 
 	global _start       ;must be declared for using gcc
@@ -47,10 +51,70 @@ _start:                     ;tell linker entry point
 	
 	mov al,10
 	call putchar
+	call putchar
 
 	mov al,8
 	mul bl
+	mov [variable], ax
 	call pHex_w
+
+	mov al,10
+	call putchar
+	call putchar
+
+	mov ax, [variable]
+	inc ax
+	mov [variable], ax
+	call pHex_w
+
+	mov al,10
+	call putchar
+	call putchar
+
+	mov ax, bx
+	mov bl, 0xFF
+	div bl
+	call pHex_b
+	mov al,10
+	call putchar
+	mov al, ah
+	call pHex_b
+
+	mov al,10
+	call putchar
+	call putchar
+
+	movzx bx, ah
+	mov ax, [variable]
+	add ax, bx
+	call pHex_w
+	mov [variable], ax
+
+	mov al,10
+	call putchar
+	call putchar
+
+	dec word [variable]
+	mov ax, [variable]
+	call pHex_w
+	mov al,10
+	call putchar
+	pushf
+	pop ax
+	call pHex_w
+
+	mov al,10
+	call putchar
+	call putchar
+
+	push 0x1503
+	pop ax
+	call pHex_w
+
+	mov al,10
+	call putchar
+	call putchar
+
 
 	mov eax, 1	;system call number (sys_exit) -- fin del programa
 	int 0x80        ;call kernel
