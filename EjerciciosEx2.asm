eccbb163638161ae864009d950d56a4c7360da57
lcgastelum5 <leonardo.cordova.gastelum@uabc.edu.mx>
2025-11-13 08:34:34 +0000

    Guardado

--- EjerciciosEx2.asm
+++ EjerciciosEx2.asm
@@ -1,17 +1,139 @@
-%include "../LIB/pc_io.inc"  	; incluir declaraciones de procedimiento externos
-								; que se encuentran en la biblioteca libpc_io.a
+%include "LIB/pc_iox.inc"
 
-section	.text
-	global _start       ;referencia para inicio de programa
-	
-_start:                   
-	mov edx, msg		; edx = dirección de la cadena msg
-	call puts			; imprime cadena msg terminada en valor nulo (0)
+N equ 2
 
-	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
-	int	0x80        	; llamada al sistema - fin de programa
+section .data
+cadena db "Hola Mundo", 0
 
-section	.data
-msg	db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
+section .bss
+length resd 1
+
+section .text
+global _start
+
+
+_start:
+	mov ebx, cadena
+	call InvertirStr
+
+
+	call PrintStr
+
+
+	mov al, 15h
+	mov cl, N 
+	call TestBit
+
+
+	mov edx, 9
+	call EsPar
+
+	mov eax, 1
+	int 0x80
+
+
+SaltoLinea:
+	push eax
+	mov al,10
+	call putchar
+	pop eax
+	ret
+
+PrintStr:
+	push edx
+	mov edx, ebx
+	call puts
+	call SaltoLinea
+	pop edx
+	ret
+
+InvertirStr:
+	push eax
+	push ecx
+	push edx
+	push esi
+
+	; calcular longitud
+	mov edx, ebx
+	mov ecx, 0
+.calc:
+	mov al, [edx]
+	cmp al, 0
+	je .res
+	inc edx
+	inc ecx
+	jmp .calc
+
+.res:
+	mov [length], ecx
+   	dec edx 
+
+	mov esi, ebx           
+	mov eax, [length]
+	add ebx, eax           
+	dec ebx                
+
+
+.inv:
+	mov al, [edx]          
+	mov [ebx], al         
+	dec edx
+	inc ebx
+	loop .inv
+
+	mov byte [ebx], 0      
+
+	mov eax, [length]
+	sub ebx, eax           
+
+	pop esi
+	pop edx
+	pop ecx
+	pop eax
+	ret
+
+
+TestBit:
+    push eax
+    push ecx
+         
+    cmp cl, 7
+    ja .rt
+
+    shr al, cl
+
+	lahf
+    and al, 1           
+    call pHex_n
+	call SaltoLinea
+
+.rt:
+      
+    pop ecx
+    pop eax
+    ret
+
+
+EsPar: 
+	push eax
+	push ebx
+	push edx
+
+	shr edx, 1
+
+	lahf  
+	mov al, ah
+	and al, 1
+	xor al, 1
+	call pHex_n
+	call SaltoLinea
+
+
+	pop edx
+	pop ebx
+	pop eax
+	ret
+
+
 
 
