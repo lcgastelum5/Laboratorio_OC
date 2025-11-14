1d502e972d18b19154251a8b0ddb2489b27ab6ea
lcgastelum5 <leonardo.cordova.gastelum@uabc.edu.mx>
2025-11-14 05:27:41 +0000

    Guardado

--- P10/P10.asm
+++ P10/P10.asm
@@ -1,10 +1,12 @@
 
 
-global Suma
+global suma
+global strleng
+global getBit
 
 section	.text
 
-Suma:
+suma:
 	push ebp
 	mov ebp, esp
 
@@ -14,3 +16,34 @@ Suma:
 	pop ebp
 	ret
 
+strleng:
+	push ebp
+	mov ebp, esp
+
+	mov eax, [ebp + 8]
+	mov ecx, 0
+	.cant: 
+		cmp byte [eax], 0
+		je .out
+		inc ecx
+		inc eax
+		jmp .cant
+	.out:
+	mov eax, ecx
+	pop ebp
+	ret
+
+getBit:
+	push ebp
+	mov ebp, esp
+
+	mov eax, [ebp + 8]
+	mov ecx, [ebp + 12]
+	inc ecx
+	shr eax, cl
+	lahf 
+	mov al, ah
+	and eax, 1
+
+	pop ebp
+	ret
--- P10/P10c.c
+++ P10/P10c.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-extern int Suma (int a, int b);
+extern int suma (int a, int b);
+extern int strleng (char *str);
+extern int getBit(int valor, int numbit);
 
 int main()
 {
@@ -8,8 +10,19 @@ int main()
 int a=2;
 int b=3;
 
-Suma(a, b);
-printf("%d", Suma);
+char str[]="Hola mundo";
+
+int valor= 9;
+int numbit=3;
+
+int total= suma(a, b);
+printf("%d\n", total);
+
+int cantidad= strleng(str);
+printf("%d\n", cantidad);
+
+int res= getBit(valor, numbit);
+printf("%d\n", res);
 
 return 0;
 }
